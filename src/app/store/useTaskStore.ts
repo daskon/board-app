2e5babf80942a4ff68b853e74db64717a1a5fd51
daskon <aselaforever@gmail.com>
2025-09-08 08:37:57 +0530

    zustand store updated. store in localstorage

--- src/app/store/useTaskStore.ts
+++ src/app/store/useTaskStore.ts
@@ -10,9 +10,25 @@ export type Task = {
 type Store = {
   tasks: Task[];
   setTasks: (tasks: Task[]) => void;
+  updateTaskStatus: (id: string, status: Task["status"]) => void;
 };
 
 export const useTaskStore = create<Store>((set) => ({
   tasks: [],
-  setTasks: (tasks) => set({ tasks }),
+  setTasks: (tasks) => {
+    set({ tasks });
+    if (typeof window !== "undefined") {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  },
+  updateTaskStatus: (id, status) =>
+    set((state) => {
+      const updated = state.tasks.map((t) =>
+        t.id === id ? { ...t, status } : t
+      );
+      if (typeof window !== "undefined") {
+        localStorage.setItem("tasks", JSON.stringify(updated));
+      }
+      return { tasks: updated };
+    }),
 }));
